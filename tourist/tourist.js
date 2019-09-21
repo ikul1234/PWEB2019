0f0a203237ae56e812dcd6b644ebdfb9fb22f80d
ikul1234 <37994966+ikul1234@users.noreply.github.com>
2019-09-21 17:42:21 +0700

    Add files via upload

---
+++ tourist/tourist.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("about");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function photofunction() {
+  var x = document.getElementById("photo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
